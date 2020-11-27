1b395726fe0ff8f8e345dd139577f05302bcb406
Afohtim <afohmouth@gmail.com>
2020-11-27 15:50:15 +0200

    Some changes of post request create_user.

--- LoginServer/login_server/views.py
+++ LoginServer/login_server/views.py
@@ -9,7 +9,7 @@ from rest_framework.views import APIView
 from django.shortcuts import redirect
 
 wrong_input = {
-    'my_error': 'invalid input'
+    'error': 'invalid input'
 }
 
 keycloak_ip = '172.18.0.4:8080'
@@ -24,32 +24,47 @@ class GetTokenView(APIView):
         token_type = request.POST.get('token_type', None)
         if passwd is None or username is None or (token_type != 'admin' and token_type != 'user'):
             return Response(wrong_input)
+        if token_type == 'admin':
+            client_id = "AdminClient"
+        elif token_type == 'user':
+            client_id = "ServerClient"
         else:
-            if token_type == 'admin':
-                client_id = "AdminClient"
-            elif token_type == 'user':
-                client_id = "ServerClient"
-            else:
-                client_id = ""
-            print(client_id)
-            data = {
-                "username": username,
-                "password": passwd,
-                "client_id": client_id,
-                "client_secret": "",
-                "grant_type": "password"
-            }
-            headers = {"Content-Type": "application/x-www-form-urlencoded"}
-            response = requests.post('http://' + keycloak_ip + '/auth/realms/messenger/protocol/openid-connect/token',
-                                     data=data, headers=headers)
+            client_id = ""
+        print(client_id)
+        data = {
+            "username": username,
+            "password": passwd,
+            "client_id": client_id,
+            "client_secret": "",
+            "grant_type": "password"
+        }
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        response = requests.post('http://' + keycloak_ip + '/auth/realms/messenger/protocol/openid-connect/token',
+                                 data=data, headers=headers)
 
-            return Response(response.json())
+        return Response(response.json())
 
 
 class CreateUserView(APIView):
     permission_classes = (AllowAny,)
 
-    def get(self, request):
+    def post(self, request):
+        username = request.POST.get('username', None)
+        email = request.POST.get('email', None)
+        passwd = request.POST.get('password', None)
+        repeat_passwd = request.POST.get('repeat_password', None)
+        token_type = request.POST.get('token_type', None)
+        if username is None or email is None or passwd is None or repeat_passwd is None or token_type is None:
+            return Response(wrong_input)
+
+        if passwd != repeat_passwd:
+            return Response({'error': 'repeat password is not equal'})
+
+        # if token_type == 'user':
+
+
+
+
         return redirect('http://localhost:8300/auth/realms/messenger/account')
 
     # TODO do registration via keycloak api
