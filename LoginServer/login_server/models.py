6326b4b248cc3f9b7222cd617c31e39512a4054b
Afohtim <afohmouth@gmail.com>
2020-11-27 16:10:19 +0200

    Add tables of users and admins

--- LoginServer/login_server/models.py
+++ LoginServer/login_server/models.py
@@ -4,8 +4,22 @@ from django.db import models
 
 
 class Users(models.Model):
-    user_name = models.CharField(max_length=60)
-    email = models.CharField(max_length=256)
+    user_name = models.CharField(max_length=60, unique=True)
+    email = models.CharField(max_length=256, unique=True)
+    password_hash = models.CharField(max_length=10000)
 
     def __str__(self):
         return self.user_name
+
+    class Meta:
+        db_table = "users"
+
+
+class Admins(models.Model):
+    admin_name = models.CharField(max_length=60, unique=True)
+    email = models.CharField(max_length=256, unique=True)
+    password_hash = models.CharField(max_length=10000)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "admin_users"
